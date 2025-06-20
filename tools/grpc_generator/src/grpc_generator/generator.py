c39850195f2b9ab3f776c0d3bc118d58af0b114e
Joe Friedrichsen <114173023+jfriedri-ni@users.noreply.github.com>
2025-06-20 11:06:39 -0700

    Hardcoded codegen PoC
    Signed-off-by: Joe Friedrichsen <114173023+jfriedri-ni@users.noreply.github.com>

--- tools/grpc_generator/src/grpc_generator/generator.py
+++ tools/grpc_generator/src/grpc_generator/generator.py
@@ -1,8 +1,10 @@
 """Generate gRPC Python stubs from proto files."""
 
 import logging
+import pathlib
 
 import click
+import grpc_tools.protoc
 
 
 _logger = logging.getLogger(__name__)
@@ -11,3 +13,82 @@ _logger.addHandler(logging.NullHandler())
 
 def handle_cli() -> None:
     """Handle the command line interface invocation."""
+    do_generation()
+
+
+def do_generation() -> None:
+    """Generate gRPC files."""
+    hardcoded_output = pathlib.Path("C:\\dev\\scratch")
+    hardcoded_include = [
+        pathlib.Path("C:\\dev\\ni\\git\\github\\ni-apis-python\\third_party\\ni-apis"),
+    ]
+    hardcoded_protos = [
+        pathlib.Path(
+            "C:\\dev\\ni\\git\\github\\ni-apis-python\\third_party\\ni-apis\\ni\\protobuf\\types\\array.proto"
+        ),
+        pathlib.Path(
+            "C:\\dev\\ni\\git\\github\\ni-apis-python\\third_party\\ni-apis\\ni\\protobuf\\types\\precision_timestamp.proto"
+        ),
+        pathlib.Path(
+            "C:\\dev\\ni\\git\\github\\ni-apis-python\\third_party\\ni-apis\\ni\\protobuf\\types\\waveform.proto"
+        ),
+        pathlib.Path(
+            "C:\\dev\\ni\\git\\github\\ni-apis-python\\third_party\\ni-apis\\ni\\protobuf\\types\\xydata.proto"
+        ),
+    ]
+
+    delete_generated_files(
+        output_path=hardcoded_output,
+    )
+    generate_python_files(
+        proto_include_paths=hardcoded_include,
+        proto_files=hardcoded_protos,
+        output_path=hardcoded_output,
+    )
+    remove_empty_service_files()
+    transform_files_for_namespace()
+
+
+def delete_generated_files(output_path: pathlib.Path) -> None:
+    """Delete all generated gRPC files to accommodate API name changes and deletions."""
+    _logger.info(
+        f"{click.style('Deleting', 'red')} old gRPC files in {click.style(str(output_path), 'bright_cyan')}"
+    )
+
+
+def generate_python_files(
+    proto_include_paths: list[pathlib.Path],
+    proto_files: list[pathlib.Path],
+    output_path: pathlib.Path,
+) -> None:
+    """Generate Python files from the Protobuf files."""
+    _logger.info(
+        f"{click.style('Generating', 'green')} new gRPC files in {click.style(str(output_path), 'bright_cyan')}"
+    )
+    _ = [_logger.info(f"  Include: {path!s}") for path in proto_include_paths]  # type: ignore[func-returns-value]
+    _ = [_logger.info(f"  Compile: {path!s}") for path in proto_files]  # type: ignore[func-returns-value]
+
+    proto_include_options = [f"--proto_path={source_path!s}" for source_path in proto_include_paths]
+    output_path_options = [
+        f"{arg_name}={output_path!s}"
+        for arg_name in ["--python_out", "--mypy_out", "--grpc_python_out", "--mypy_grpc_out"]
+    ]
+    proto_file_args = [f"{proto_file!s}" for proto_file in proto_files]
+
+    protoc_arguments = ["protoc"]
+    protoc_arguments.extend(proto_include_options)
+    protoc_arguments.extend(output_path_options)
+    protoc_arguments.extend(proto_file_args)
+
+    _logger.info(f"  Invoking '{click.style(' '.join(protoc_arguments), 'bright_white')}'")
+    grpc_tools.protoc.main(protoc_arguments)
+
+
+def remove_empty_service_files() -> None:
+    """Detect and remove empty '..._grpc.py' stubs files."""
+    _logger.info(f"{click.style('Removing', 'yellow')} empty gRPC service files.")
+
+
+def transform_files_for_namespace() -> None:
+    """Convert a submodule to a subpackage."""
+    _logger.info(f"{click.style('Transforming', 'yellow')} gRPC submodules to subpackages")
