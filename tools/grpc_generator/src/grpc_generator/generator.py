9acd1f144d41c168f7dd27cbce470fb04aeedcf6
Joe Friedrichsen <114173023+jfriedri-ni@users.noreply.github.com>
2025-06-20 15:42:57 -0700

    Generate the protobuf descriptor set in the same call that generates the package - Add utility methods to GenerationSpec for inspecting a generated protobuf package
    Signed-off-by: Joe Friedrichsen <114173023+jfriedri-ni@users.noreply.github.com>

--- tools/grpc_generator/src/grpc_generator/generator.py
+++ tools/grpc_generator/src/grpc_generator/generator.py
@@ -18,6 +18,25 @@ class GenerationSpec(NamedTuple):
     include_paths: list[pathlib.Path]
     output_path: pathlib.Path
 
+    @property
+    def package_descriptor_file(self) -> pathlib.Path:
+        """Return the path for the package's FileDescriptorSet."""
+        return self.output_path.joinpath(f"{self.name}-descriptor.pb")
+
+    def get_matching_message_files(self, relative_path: pathlib.Path) -> list[pathlib.Path]:
+        """Get the full paths to the generated message files for the specified proto package path."""
+        full_path = self.output_path.joinpath(relative_path)
+        logic_file = full_path.with_name(f"{full_path.stem}_pb2.py")
+        types_file = full_path.with_name(f"{full_path.stem}_pb2.pyi")
+        return [logic_file, types_file]
+
+    def get_matching_service_files(self, relative_path: pathlib.Path) -> list[pathlib.Path]:
+        """Get the full paths to the generated service files for the specified proto package path."""
+        full_path = self.output_path.joinpath(relative_path)
+        logic_file = full_path.with_name(f"{full_path.stem}_pb2_grpc.py")
+        types_file = full_path.with_name(f"{full_path.stem}_pb2_grpc.pyi")
+        return [logic_file, types_file]
+
 
 _logger = logging.getLogger(__name__)
 _logger.addHandler(logging.NullHandler())
@@ -57,7 +76,7 @@ def do_generation() -> None:
     )
 
     device_spec = GenerationSpec(
-        name="ni.grpcdevice.v1",
+        name="nidevice_grpc",
         proto_paths=[pathlib.Path("C:\\dev\\ni\\git\\github\\ni-apis-python\\third_party\\ni-apis\\ni\\grpcdevice\\v1\\session.proto")],
         include_paths=[pathlib.Path("C:\\dev\\ni\\git\\github\\ni-apis-python\\third_party\\ni-apis")],
         output_path=hardcoded_output
@@ -96,10 +115,13 @@ def generate_python_files(generation_spec: GenerationSpec) -> None:
     ]
     proto_file_args = [f"{proto_file!s}" for proto_file in generation_spec.proto_paths]
 
-    protoc_arguments = ["protoc"]
-    protoc_arguments.extend(proto_include_options)
-    protoc_arguments.extend(output_path_options)
-    protoc_arguments.extend(proto_file_args)
+    protoc_arguments = [
+        "protoc",
+        *proto_include_options,
+        *output_path_options,
+        f"--descriptor_set_out={generation_spec.package_descriptor_file!s}",
+        *proto_file_args,
+    ]
 
     invoke_protoc(protoc_arguments)
 
@@ -110,46 +132,35 @@ def remove_empty_service_files(generation_spec: GenerationSpec) -> None:
         f"{click.style('Removing', 'yellow')} empty gRPC service files in {click.style(str(generation_spec.output_path), 'bright_cyan')}."
     )
 
-    # move to temp folder
-    proto_include_options = [
-        f"--proto_path={source_path!s}" for source_path in generation_spec.include_paths
-    ]
-    proto_file_args = [f"{proto_file!s}" for proto_file in generation_spec.proto_paths]
-    proto_descriptor_file = f"{generation_spec.name}-descriptor.pb"
-    protoc_arguments = [
-        "protoc",
-        *proto_include_options,
-        f"--descriptor_set_out={proto_descriptor_file}",
-        "--include_imports",
-        *proto_file_args,
-    ]
-    invoke_protoc(protoc_arguments)
-    inspect_proto_descriptor(str(proto_descriptor_file))
+    inspect_proto_descriptor(generation_spec)
 
 
-def inspect_proto_descriptor(descriptor_path: str) -> None:
+def inspect_proto_descriptor(generation_spec: GenerationSpec) -> None:
     # Read the descriptor file
     print()
-    print(f"analyzing {descriptor_path}")
-    with open(descriptor_path, "rb") as f:
-        desc_set = descriptor_pb2.FileDescriptorSet()
-        desc_set.ParseFromString(f.read())
+    print(f"============== analyzing {generation_spec.package_descriptor_file} ==============")
+    with open(generation_spec.package_descriptor_file, "rb") as f:
+        package_descriptor_set = descriptor_pb2.FileDescriptorSet()
+        package_descriptor_set.ParseFromString(f.read())
 
-    for file_desc in desc_set.file:
-        print(f"== File: {file_desc.name} ==")
-        if file_desc.package:
-            print(f"  Package: {file_desc.package}")
+    for file_descriptor in package_descriptor_set.file:
+        relative_path = pathlib.Path(file_descriptor.name)
+        print(f"== File: {relative_path!s} ==")
+        if file_descriptor.package:
+            print(f"  Package: {file_descriptor.package}")
+            print(f"    Messages: {generation_spec.get_matching_message_files(relative_path)}")
+            print(f"    Services: {generation_spec.get_matching_service_files(relative_path)}")
 
         # List messages
-        if file_desc.message_type:
+        if file_descriptor.message_type:
             print("  Messages:")
-            for msg in file_desc.message_type:
+            for msg in file_descriptor.message_type:
                 print(f"    - {msg.name}")
 
         # List services
-        if file_desc.service:
+        if file_descriptor.service:
             print("  Services:")
-            for svc in file_desc.service:
+            for svc in file_descriptor.service:
                 print(f"    - {svc.name}")
 
         print()
