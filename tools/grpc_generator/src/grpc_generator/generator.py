c6deae37dc171b163601a3d6fe678aa271a42258
Joe Friedrichsen <114173023+jfriedri-ni@users.noreply.github.com>
2025-06-20 08:46:07 -0700

    Remove bootstrap message
    Signed-off-by: Joe Friedrichsen <114173023+jfriedri-ni@users.noreply.github.com>

--- tools/grpc_generator/src/grpc_generator/generator.py
+++ tools/grpc_generator/src/grpc_generator/generator.py
@@ -11,12 +11,3 @@ _logger.addHandler(logging.NullHandler())
 
 def handle_cli() -> None:
     """Handle the command line interface invocation."""
-    color_world = {
-        "W": "red",
-        "o": "yellow",
-        "r": "green",
-        "l": "blue",
-        "d": "magenta",
-    }
-    message = "".join([click.style(text, color) for text, color in color_world.items()])
-    click.echo(f"Hello {message}!")
