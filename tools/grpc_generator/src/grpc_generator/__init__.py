426bd3e0de7bd2cf9c64962b10f8c6023e261eee
Joe Friedrichsen <114173023+jfriedri-ni@users.noreply.github.com>
2025-06-26 08:34:04 -0700

    Filter the grpc_tools UserWarning explicitly
    Signed-off-by: Joe Friedrichsen <114173023+jfriedri-ni@users.noreply.github.com>

--- tools/grpc_generator/src/grpc_generator/__init__.py
+++ tools/grpc_generator/src/grpc_generator/__init__.py
@@ -3,5 +3,7 @@
 import warnings
 
 warnings.filterwarnings(
-    "ignore"
+    action="ignore",
+    category=UserWarning,
+    module=r"^grpc_tools",
 )  # grpc_tools\protoc.py:21: UserWarning: pkg_resources is deprecated as an API
