a9e5f3dfec24c3b3057368a12327d2614c9e9636
Michael Johansen <michael.johansen@ni.com>
2025-07-11 13:55:36 -0500

    Add conversion functions for waveform and precision timestamp.
    Signed-off-by: Michael Johansen <michael.johansen@ni.com>

---
+++ packages/ni.protobuf.types/src/ni/protobuf/types/precision_timestamp_converters.py
@@ -0,0 +1,23 @@
+"""Methods to convert to and from PrecisionTimestamp protobuf messages."""
+
+from __future__ import annotations
+
+import nitypes.bintime as bt
+
+from ni.protobuf.types.precision_timestamp_pb2 import (
+    PrecisionTimestamp,
+)
+
+
+def bintime_datetime_to_protobuf_message(python_value: bt.DateTime) -> PrecisionTimestamp:
+    """Convert the Python DateTime to a protobuf PrecisionTimestamp."""
+    seconds, fractional_seconds = python_value.to_tuple()
+    return PrecisionTimestamp(seconds=seconds, fractional_seconds=fractional_seconds)
+
+
+def precision_timestamp_to_python_value(protobuf_message: PrecisionTimestamp) -> bt.DateTime:
+    """Convert the protobuf PrecisionTimestamp to a Python DateTime."""
+    time_value_tuple = bt.TimeValueTuple(
+        protobuf_message.seconds, protobuf_message.fractional_seconds
+    )
+    return bt.DateTime.from_tuple(time_value_tuple)
---
+++ packages/ni.protobuf.types/src/ni/protobuf/types/waveform_converters.py
@@ -0,0 +1,116 @@
+"""Methods to convert to and from waveform protobuf messages."""
+
+from __future__ import annotations
+
+import datetime as dt
+from collections.abc import Mapping
+
+import hightime as ht
+import nitypes.bintime as bt
+import numpy as np
+from nitypes.time import convert_datetime
+from nitypes.waveform import (
+    AnalogWaveform,
+    ExtendedPropertyDictionary,
+    ExtendedPropertyValue,
+    NoneScaleMode,
+    Timing,
+)
+
+from ni.protobuf.types.precision_timestamp_converters import (
+    bintime_datetime_to_protobuf_message,
+    precision_timestamp_to_python_value,
+)
+from ni.protobuf.types.waveform_pb2 import (
+    DoubleAnalogWaveform,
+    WaveformAttributeValue,
+)
+
+
+def analog_waveform_to_protobuf_message(
+    python_value: AnalogWaveform[np.float64],
+) -> DoubleAnalogWaveform:
+    """Convert the Python AnalogWaveform to a protobuf DoubleAnalogWaveform."""
+    if python_value.timing.has_start_time:
+        bin_datetime = convert_datetime(bt.DateTime, python_value.timing.start_time)
+        precision_timestamp = bintime_datetime_to_protobuf_message(bin_datetime)
+    else:
+        precision_timestamp = None
+
+    if python_value.timing.has_sample_interval:
+        time_interval = python_value.timing.sample_interval.total_seconds()
+    else:
+        time_interval = 0
+
+    attributes = _extended_properties_to_attributes(python_value.extended_properties)
+
+    return DoubleAnalogWaveform(
+        t0=precision_timestamp,
+        dt=time_interval,
+        y_data=python_value.scaled_data,
+        attributes=attributes,
+    )
+
+
+def double_analog_waveform_to_python_value(
+    protobuf_message: DoubleAnalogWaveform,
+) -> AnalogWaveform[np.float64]:
+    """Convert the protobuf DoubleAnalogWaveform to a Python AnalogWaveform."""
+    # Declare timing to accept both bintime and dt.datetime to satisfy mypy.
+    timing: Timing[bt.DateTime | dt.datetime]
+    if not protobuf_message.dt and not protobuf_message.HasField("t0"):
+        # If both dt and t0 are unset, use Timing.empty.
+        timing = Timing.empty
+    else:
+        # Timestamp
+        bin_datetime = precision_timestamp_to_python_value(protobuf_message.t0)
+
+        # Sample Interval
+        if not protobuf_message.dt:
+            timing = Timing.create_with_no_interval(timestamp=bin_datetime)
+        else:
+            sample_interval = ht.timedelta(seconds=protobuf_message.dt)
+            timing = Timing.create_with_regular_interval(
+                sample_interval=sample_interval,
+                timestamp=bin_datetime,
+            )
+
+    extended_properties = {}
+    for key, value in protobuf_message.attributes.items():
+        attr_type = value.WhichOneof("attribute")
+        extended_properties[key] = getattr(value, str(attr_type))
+
+    data_array = np.array(protobuf_message.y_data)
+    return AnalogWaveform(
+        sample_count=data_array.size,
+        dtype=np.float64,
+        raw_data=data_array,
+        start_index=0,
+        capacity=data_array.size,
+        extended_properties=extended_properties,
+        copy_extended_properties=True,
+        timing=timing,
+        scale_mode=NoneScaleMode(),
+    )
+
+
+def _extended_properties_to_attributes(
+    extended_properties: ExtendedPropertyDictionary,
+) -> Mapping[str, WaveformAttributeValue]:
+    return {key: _value_to_attribute(value) for key, value in extended_properties.items()}
+
+
+def _value_to_attribute(value: ExtendedPropertyValue) -> WaveformAttributeValue:
+    attr_value = WaveformAttributeValue()
+    if isinstance(value, bool):
+        attr_value.bool_value = value
+    elif isinstance(value, int):
+        attr_value.integer_value = value
+    elif isinstance(value, float):
+        attr_value.double_value = value
+    elif isinstance(value, str):
+        attr_value.string_value = value
+    else:
+        raise TypeError(f"Unexpected type for extended property value {type(value)}")
+
+    return attr_value
