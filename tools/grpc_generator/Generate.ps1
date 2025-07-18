d35358007641b79da3e45f17febaa0f181853172
Peter Kurlak <peter.kurlak@emerson.com>
2025-07-17 20:43:42 -0500

    Create grpc generation PowerShell script

---
+++ tools/grpc_generator/Generate.ps1
@@ -0,0 +1,17 @@
+$packages = @(
+    "ni\datamonikers\v1"
+    "ni\measurementlink\discovery\v1"
+    "ni\measurementlink\measurement\v3"
+    "ni\measurements\data\v1"
+    "ni\measurements\metadata\v1"
+    "ni\protobuf\types"
+
+)
+
+$packages | Foreach-Object {
+    & poetry run grpc-generator `
+        --proto-basepath ..\..\third_party\ni-apis `
+        --proto-subpath $_ `
+        --output-basepath ..\..\packages\$($_ -replace '\\', '.')\src `
+        --output-format submodule
+}
