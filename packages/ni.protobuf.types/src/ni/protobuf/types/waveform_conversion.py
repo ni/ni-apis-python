7aa81fa30d6f2ecc9b46897feb9cee3f553f3437
mjohanse-emr <michael.johansen@emerson.com>
2025-07-21 14:23:00 -0500

    Raise an error if irregular timing is encountered during conversion. (#31)
    * Raise an error if irregular timing is encountered during conversion.
    
    Signed-off-by: Michael Johansen <michael.johansen@ni.com>
    
    * Fix typo.
    
    Signed-off-by: Michael Johansen <michael.johansen@ni.com>
    
    * Rename method based on PR feedback.
    
    Signed-off-by: Michael Johansen <michael.johansen@ni.com>
    
    ---------
    
    Signed-off-by: Michael Johansen <michael.johansen@ni.com>
    Co-authored-by: Michael Johansen <michael.johansen@ni.com>

--- packages/ni.protobuf.types/src/ni/protobuf/types/waveform_conversion.py
+++ packages/ni.protobuf.types/src/ni/protobuf/types/waveform_conversion.py
@@ -18,6 +18,7 @@ from nitypes.waveform import (
     ExtendedPropertyValue,
     LinearScaleMode,
     NoneScaleMode,
+    SampleIntervalMode,
     Spectrum,
     Timing,
 )
@@ -42,6 +43,7 @@ def float64_analog_waveform_to_protobuf(
     value: AnalogWaveform[np.float64], /
 ) -> DoubleAnalogWaveform:
     """Convert the Python AnalogWaveform to a protobuf DoubleAnalogWaveform."""
+    _validate_timing(value)
     t0 = _t0_from_waveform(value)
     time_interval = _time_interval_from_waveform(value)
     attributes = _extended_properties_to_attributes(value.extended_properties)
@@ -74,6 +76,7 @@ def float64_complex_waveform_to_protobuf(
     value: ComplexWaveform[np.complex128], /
 ) -> DoubleComplexWaveform:
     """Convert the Python ComplexWaveform to a protobuf DoubleComplexWaveform."""
+    _validate_timing(value)
     t0 = _t0_from_waveform(value)
     time_interval = _time_interval_from_waveform(value)
     attributes = _extended_properties_to_attributes(value.extended_properties)
@@ -111,6 +114,7 @@ def int16_complex_waveform_to_protobuf(
     value: ComplexWaveform[ComplexInt32Base], /
 ) -> I16ComplexWaveform:
     """Convert the Python ComplexWaveform to a protobuf DoubleComplexWaveform."""
+    _validate_timing(value)
     t0 = _t0_from_waveform(value)
     time_interval = _time_interval_from_waveform(value)
     attributes = _extended_properties_to_attributes(value.extended_properties)
@@ -203,6 +207,13 @@ def _value_to_attribute(value: ExtendedPropertyValue) -> WaveformAttributeValue:
         raise TypeError(f"Unexpected type for extended property value {type(value)}")
 
     return attr_value
+
+
+def _validate_timing(
+    waveform: AnalogWaveform[Any] | ComplexWaveform[Any],
+) -> None:
+    if waveform.timing.sample_interval_mode == SampleIntervalMode.IRREGULAR:
+        raise ValueError("Cannot convert irregular sample interval to protobuf.")
 
 
 def _t0_from_waveform(
