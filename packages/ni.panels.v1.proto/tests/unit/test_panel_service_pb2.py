5a7827db31741f8499d85c62198d6f7b037b2a93
Michael Johansen <michael.johansen@ni.com>
2025-08-03 12:42:18 -0500

    Apply review feedback to unit tests.
    Signed-off-by: Michael Johansen <michael.johansen@ni.com>

--- packages/ni.panels.v1.proto/tests/unit/test_panel_service_pb2.py
+++ packages/ni.panels.v1.proto/tests/unit/test_panel_service_pb2.py
@@ -1,20 +1,39 @@
-from google.protobuf.any_pb2 import Any
+import google.protobuf.any_pb2 as any_pb2
+from typing_extensions import assert_type
+import ni.panels.v1.panel_service_pb2
 from ni.panels.v1.panel_service_pb2 import (
-    EnumeratePanelsRequest,
     EnumeratePanelsResponse,
     GetValueRequest,
     GetValueResponse,
     PanelInformation,
     SetValueRequest,
-    SetValueResponse,
     StartPanelRequest,
     StartPanelResponse,
     StopPanelRequest,
-    StopPanelResponse,
     TryGetValueRequest,
     TryGetValueResponse,
 )
 from ni.panels.v1.streamlit_panel_configuration_pb2 import StreamlitPanelConfiguration
+
+
+def test___panel_service_module___check_messages___has_correct_messages() -> None:
+    expected_message_types = [
+        "EnumeratePanelsRequest",
+        "EnumeratePanelsResponse",
+        "GetValueRequest",
+        "GetValueResponse",
+        "PanelInformation",
+        "SetValueRequest",
+        "SetValueResponse",
+        "StartPanelRequest",
+        "StartPanelResponse",
+        "StopPanelRequest",
+        "StopPanelResponse",
+        "TryGetValueRequest",
+        "TryGetValueResponse",
+    ]
+    for message_type in expected_message_types:
+        assert hasattr(ni.panels.v1.panel_service_pb2, message_type)
 
 
 def test___start_panel_request___create___created_successfully() -> None:
@@ -39,18 +58,6 @@ def test___stop_panel_request___create___created_successfully() -> None:
     assert req.reset is True
 
 
-def test___stop_panel_response___create___created_successfully() -> None:
-    req = StopPanelResponse()
-
-    assert isinstance(req, StopPanelResponse)
-
-
-def test___enumerate_panels_request___create___created_successfully() -> None:
-    req = EnumeratePanelsRequest()
-
-    assert isinstance(req, EnumeratePanelsRequest)
-
-
 def test___enumerate_panels_response___create___created_successfully() -> None:
     panel_info = PanelInformation(
         panel_id="panel_id",
@@ -59,11 +66,13 @@ def test___enumerate_panels_response___create___created_successfully() -> None:
     )
     req = EnumeratePanelsResponse(panels=[panel_info])
 
-    panels = list(req.panels)
-    assert len(panels) == 1
-    assert panels[0].panel_id == "panel_id"
-    assert panels[0].panel_url == "some url"
-    assert panels[0].value_ids == ["val1", "val2"]
+    assert len(req.panels) == 1
+    panel_info = req.panels[0]
+    assert_type(panel_info, PanelInformation)
+    assert isinstance(panel_info, PanelInformation)
+    assert panel_info.panel_id == "panel_id"
+    assert panel_info.panel_url == "some url"
+    assert panel_info.value_ids == ["val1", "val2"]
 
 
 def test___get_value_request___create___created_successfully() -> None:
@@ -110,14 +119,8 @@ def test___set_value_request___create___created_successfully() -> None:
     assert req.notify is True
 
 
-def test___set_value_response___create___created_successfully() -> None:
-    req = SetValueResponse()
-
-    assert isinstance(req, SetValueResponse)
-
-
-def _get_configuration_as_any() -> Any:
+def _get_configuration_as_any() -> any_pb2.Any:
     configuration = StreamlitPanelConfiguration(panel_script_url="path/to/panel.py")
-    configuration_any = Any()
+    configuration_any = any_pb2.Any()
     configuration_any.Pack(configuration)
     return configuration_any
