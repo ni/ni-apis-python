70a733b8b74f763aca5953a77173202e081d688c
Joel Dixon <joel.dixon@ni.com>
2025-08-13 11:20:48 -0500

    Add multiple packages from ni-apis

---
+++ tools/grpc_generator/Generate.ps1
@@ -0,0 +1,15 @@
+$packages = @(
+    "ni\measurementlink\discovery\v1"
+    "ni\measurementlink\measurement\v2"
+    "ni\measurementlink"
+    "ni\measurementlink\pinmap\v1"
+    "ni\protobuf\types"
+)
+
+$packages | Foreach-Object {
+    & poetry run grpc-generator `
+        --proto-basepath ..\..\third_party\ni-apis `
+        --proto-subpath $_ `
+        --output-basepath ..\..\packages\$($_ -replace '\\', '.')\src `
+        --output-format submodule
+}
