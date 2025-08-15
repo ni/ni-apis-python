0f4ab6a31cc30a0f2a53ef66b96f0e1a9749f5b2
Joel Dixon <joel.dixon@ni.com>
2025-08-15 16:03:03 -0500

    Lint fixes for new unit tests

--- packages/ni.measurementlink.measurement.v1.proto/tests/unit/test_ni_measurementlink_measurement_v1.py
+++ packages/ni.measurementlink.measurement.v1.proto/tests/unit/test_ni_measurementlink_measurement_v1.py
@@ -1,36 +1,39 @@
 """Tests for the ni.measurementlink.proto package."""
 
-from ni.measurementlink.measurement.v1.measurement_service_pb2 import(
+from ni.measurementlink.measurement.v1.measurement_service_pb2 import (
     GetMetadataRequest,
     GetMetadataResponse,
     MeasurementDetails,
     MeasurementSignature,
 )
 
+
 def test___creategetmetadatarequest___message_created() -> None:
     request = GetMetadataRequest()
     assert request is not None
+
 
 def test___creategetmetadataresponse___message_created() -> None:
     response = GetMetadataResponse()
     assert response is not None
 
+
 def test___getmetadataresponse___with_measurementdetails___retrieved() -> None:
     response = GetMetadataResponse(
-        measurement_details = MeasurementDetails(
-            display_name = "Test Name",
-            version = "1.2.3"
-        )
+        measurement_details=MeasurementDetails(display_name="Test Name", version="1.2.3")
     )
     assert response.measurement_details.display_name == "Test Name"
     assert response.measurement_details.version == "1.2.3"
 
+
 def test___getmetadataresponse___with_measurementsignature___retrieved() -> None:
     response = GetMetadataResponse(
-        measurement_signature = MeasurementSignature(
-            configuration_parameters_message_type = "Test Message Type",
-            outputs_message_type = "Test Outputs Message Type"
+        measurement_signature=MeasurementSignature(
+            configuration_parameters_message_type="Test Message Type",
+            outputs_message_type="Test Outputs Message Type",
         )
     )
-    assert response.measurement_signature.configuration_parameters_message_type == "Test Message Type"
+    assert (
+        response.measurement_signature.configuration_parameters_message_type == "Test Message Type"
+    )
     assert response.measurement_signature.outputs_message_type == "Test Outputs Message Type"
