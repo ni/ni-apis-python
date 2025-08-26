fc93661326ae476b7ff065ecf49c2807933eee99
Joel Dixon <joel.dixon@ni.com>
2025-08-26 15:11:06 -0500

    Fix the auto-generation of API Reference for ni.grpcdevice.v1.proto

--- packages/ni.grpcdevice.v1.proto/docs/conf.py
+++ packages/ni.grpcdevice.v1.proto/docs/conf.py
@@ -46,7 +46,8 @@ htmlhelp_basename = f"{project}doc"
 autoapi_options = list(autoapi.extension._DEFAULT_OPTIONS)
 autoapi_options.remove("private-members")  # note: remove this to include "_" members in docs
 autoapi_dirs = [
-    root_path / "src"
+    root_path / "src" / "session_pb2",
+    root_path / "src" / "session_pb2_grpc",
 ]
 autoapi_python_use_implicit_namespaces = True
 autoapi_template_dir = "templates/autoapi"
@@ -54,9 +55,6 @@ autoapi_python_class_content = "both"
 autoapi_type = "python"
 autodoc_typehints = "description"
 autoapi_file_patterns = ["*.pyi", "*.py"]
-autoapi_ignore = [
-    '**/__init__.py',
-]
 
 
 def process_docstring(app, what, name, obj, options, lines):
--- packages/ni.grpcdevice.v1.proto/docs/index.rst
+++ packages/ni.grpcdevice.v1.proto/docs/index.rst
@@ -11,8 +11,6 @@ Table of Contents
    :maxdepth: 4
 
    autoapi/index
-   autoapi/src/session_pb2/index
-   autoapi/src/session_pb2_grpc/index
 
 Indices and tables
 ==================
