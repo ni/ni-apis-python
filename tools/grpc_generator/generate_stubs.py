998c479eb6391977b1920bb4f77fc8407d0d8cdf
hunter-ni <68388297+hunter-ni@users.noreply.github.com>
2025-08-27 13:47:40 -0500

    Review feedback - Switch from using open() to using path.read_text(). Update name of 'repo_root' local variable to 'repo_root_path' for consistency.

--- tools/grpc_generator/generate_stubs.py
+++ tools/grpc_generator/generate_stubs.py
@@ -7,22 +7,21 @@ from pathlib import Path
 
 def main():
     """Executes the generation of the gRPC stubs based on the specified package information."""
-    repo_root = Path(__file__).parent.parent.parent
+    repo_root_path = Path(__file__).parent.parent.parent
 
-    packages_file_path = repo_root / "packages.json"
+    packages_file_path = repo_root_path / "packages.json"
 
-    with open(packages_file_path, "r", encoding="utf-8") as packages_file:
-        packages = json.load(packages_file)
+    packages = json.loads(packages_file_path.read_text(encoding="utf-8"))
 
     for package_name, package_info in packages.items():
         # Skip package entries with no specified output format
         if not package_info.get("output-format"):
             continue
 
-        proto_basepath = repo_root.joinpath(package_info["proto-basepath"])
+        proto_basepath = repo_root_path.joinpath(package_info["proto-basepath"])
         proto_subpath = Path(package_info["proto-subpath"])
-        proto_include_path = repo_root.joinpath(package_info["proto-include-path"])
-        output_basepath = repo_root.joinpath(f"./packages/{package_name}/src")
+        proto_include_path = repo_root_path.joinpath(package_info["proto-include-path"])
+        output_basepath = repo_root_path.joinpath(f"./packages/{package_name}/src")
         output_format = package_info["output-format"]
 
         args = [
