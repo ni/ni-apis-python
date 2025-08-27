54756ee2bbfac1027d4f3c2513f91f8955ae3551
hunter-ni <68388297+hunter-ni@users.noreply.github.com>
2025-08-27 12:30:50 -0500

    Applying linter-related fixes.

--- tools/grpc_generator/generate_stubs.py
+++ tools/grpc_generator/generate_stubs.py
@@ -1,19 +1,25 @@
+"""Script to generate gRPC stubs for all packages in the repository."""
+
 import json
 import os
 import subprocess
 
-def get_path_from_root_directory_relative_path(root_directory_relative_path) -> str:
+
+def _get_path_from_root_directory_relative_path(root_directory_relative_path) -> str:
     return os.path.join(
         os.path.dirname(__file__),
         "..",
         "..",
-        convert_path_to_use_os_path_separators(root_directory_relative_path))
+        _convert_path_to_use_os_path_separators(root_directory_relative_path),
+    )
 
-def convert_path_to_use_os_path_separators(path) -> str:
-    return path.replace('/', os.path.sep)
+
+def _convert_path_to_use_os_path_separators(path) -> str:
+    return path.replace("/", os.path.sep)
+
 
 packages_file_name = "packages.json"
-packages_file_path = get_path_from_root_directory_relative_path(packages_file_name)
+packages_file_path = _get_path_from_root_directory_relative_path(packages_file_name)
 
 with open(packages_file_path, "r", encoding="utf-8") as packages_file:
     packages = json.load(packages_file)
@@ -23,19 +29,28 @@ for package_name, package_info in packages.items():
     if not package_info.get("output-format"):
         continue
 
-    proto_basepath = get_path_from_root_directory_relative_path(package_info["proto-basepath"])
-    proto_subpath = convert_path_to_use_os_path_separators(package_info["proto-subpath"])
-    proto_include_path = get_path_from_root_directory_relative_path(package_info["proto-include-path"])
-    output_basepath = get_path_from_root_directory_relative_path(f"./packages/{package_name}/src")
+    proto_basepath = _get_path_from_root_directory_relative_path(package_info["proto-basepath"])
+    proto_subpath = _convert_path_to_use_os_path_separators(package_info["proto-subpath"])
+    proto_include_path = _get_path_from_root_directory_relative_path(
+        package_info["proto-include-path"]
+    )
+    output_basepath = _get_path_from_root_directory_relative_path(f"./packages/{package_name}/src")
     output_format = package_info["output-format"]
 
     args = [
-        "poetry", "run", "grpc-generator",
-        "--proto-basepath", proto_basepath,
-        "--proto-subpath", proto_subpath,
-        "--proto-include-path", proto_include_path,
-        "--output-basepath", output_basepath,
-        "--output-format", output_format
+        "poetry",
+        "run",
+        "grpc-generator",
+        "--proto-basepath",
+        proto_basepath,
+        "--proto-subpath",
+        proto_subpath,
+        "--proto-include-path",
+        proto_include_path,
+        "--output-basepath",
+        output_basepath,
+        "--output-format",
+        output_format,
     ]
 
     print(f"Generating stubs for {package_name}...")
