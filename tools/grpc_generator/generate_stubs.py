8736eb0829c1507e41edfcbbf75d13d1f7e0c4da
hunter-ni <68388297+hunter-ni@users.noreply.github.com>
2025-08-27 13:39:44 -0500

    Reivew feedback -- define a main function.

--- tools/grpc_generator/generate_stubs.py
+++ tools/grpc_generator/generate_stubs.py
@@ -5,41 +5,47 @@ import subprocess
 from pathlib import Path
 
 
-_REPO_ROOT = Path(__file__).parent.parent.parent
+def main():
+    """Executes the generation of the gRPC stubs based on the specified package information."""
+    repo_root = Path(__file__).parent.parent.parent
 
-packages_file_path = _REPO_ROOT / "packages.json"
+    packages_file_path = repo_root / "packages.json"
 
-with open(packages_file_path, "r", encoding="utf-8") as packages_file:
-    packages = json.load(packages_file)
+    with open(packages_file_path, "r", encoding="utf-8") as packages_file:
+        packages = json.load(packages_file)
 
-for package_name, package_info in packages.items():
-    # Skip package entries with no specified output format
-    if not package_info.get("output-format"):
-        continue
+    for package_name, package_info in packages.items():
+        # Skip package entries with no specified output format
+        if not package_info.get("output-format"):
+            continue
 
-    proto_basepath = _REPO_ROOT.joinpath(package_info["proto-basepath"])
-    proto_subpath = Path(package_info["proto-subpath"])
-    proto_include_path = _REPO_ROOT.joinpath(package_info["proto-include-path"])
-    output_basepath = _REPO_ROOT.joinpath(f"./packages/{package_name}/src")
-    output_format = package_info["output-format"]
+        proto_basepath = repo_root.joinpath(package_info["proto-basepath"])
+        proto_subpath = Path(package_info["proto-subpath"])
+        proto_include_path = repo_root.joinpath(package_info["proto-include-path"])
+        output_basepath = repo_root.joinpath(f"./packages/{package_name}/src")
+        output_format = package_info["output-format"]
 
-    args = [
-        "poetry",
-        "run",
-        "grpc-generator",
-        "--proto-basepath",
-        proto_basepath,
-        "--proto-subpath",
-        proto_subpath,
-        "--proto-include-path",
-        proto_include_path,
-        "--output-basepath",
-        output_basepath,
-        "--output-format",
-        output_format,
-    ]
+        args = [
+            "poetry",
+            "run",
+            "grpc-generator",
+            "--proto-basepath",
+            proto_basepath,
+            "--proto-subpath",
+            proto_subpath,
+            "--proto-include-path",
+            proto_include_path,
+            "--output-basepath",
+            output_basepath,
+            "--output-format",
+            output_format,
+        ]
 
-    print(f"Generating stubs for {package_name}...")
-    print("------------------------------------------------------------------------")
-    subprocess.run(args, check=True)
-    print()
+        print(f"Generating stubs for {package_name}...")
+        print("------------------------------------------------------------------------")
+        subprocess.run(args, check=True)
+        print()
+
+
+if __name__ == "__main__":
+    main()
