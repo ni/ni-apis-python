0664f53db6f6dfa66a35b60f2c3dd248d7623d93
Joel Dixon <joel.dixon@ni.com>
2025-08-27 10:34:51 -0500

    Fix ni.measurementlink.sessionmanagement.v1 docs

--- packages/ni.measurementlink.sessionmanagement.v1.proto/docs/conf.py
+++ packages/ni.measurementlink.sessionmanagement.v1.proto/docs/conf.py
@@ -45,17 +45,13 @@ htmlhelp_basename = f"{project}doc"
 # tell autoapi to doc the public options
 autoapi_options = list(autoapi.extension._DEFAULT_OPTIONS)
 autoapi_options.remove("private-members")  # note: remove this to include "_" members in docs
-autoapi_dirs = [
-    root_path / "src" / "ni",
-]
+autoapi_dirs = [root_path / "src" / "ni"]
 autoapi_python_use_implicit_namespaces = True
+autoapi_template_dir = "templates/autoapi"
 autoapi_python_class_content = "both"
 autoapi_type = "python"
 autodoc_typehints = "description"
 autoapi_file_patterns = ["*.pyi", "*.py"]
-autoapi_ignore = [
-    '**/__init__.py',
-]
 
 
 def process_docstring(app, what, name, obj, options, lines):
--- packages/ni.measurementlink.sessionmanagement.v1.proto/docs/index.rst
+++ packages/ni.measurementlink.sessionmanagement.v1.proto/docs/index.rst
@@ -11,8 +11,6 @@ Table of Contents
    :maxdepth: 4
 
    autoapi/index
-   autoapi/ni/measurementlink/sessionmanagement/v1/session_management_service_pb2/index
-   autoapi/ni/measurementlink/sessionmanagement/v1/session_management_service_pb2_grpc/index
 
 Indices and tables
 ==================
