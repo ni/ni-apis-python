d5b700021c5429aed370d46b70b2681d39e190ba
hunter-ni <68388297+hunter-ni@users.noreply.github.com>
2025-09-08 10:54:44 -0500

    Update service class for DataMonikers client package

--- packages/ni.datamonikers.v1.client/src/ni/datamonikers/v1/client/_client.py
+++ packages/ni.datamonikers.v1.client/src/ni/datamonikers/v1/client/_client.py
@@ -3,7 +3,7 @@
 import logging
 
 GRPC_SERVICE_INTERFACE_NAME = "ni.datamonikers.v1.MonikerService"
-GRPC_SERVICE_CLASS = "ni.datamonikers.v1.MonikerService"
+GRPC_SERVICE_CLASS = "ni.measurements.data.v1.DataStoreService"
 
 _logger = logging.getLogger(__name__)
 
