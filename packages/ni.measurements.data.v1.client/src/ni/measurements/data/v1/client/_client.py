15668cd5d4617034276a1a0f54b8816c1639acea
hunter-ni <68388297+hunter-ni@users.noreply.github.com>
2025-09-08 14:51:56 -0500

    Adding initial implementations of DataStoreClient and MetadataStoreClient

--- packages/ni.measurements.data.v1.client/src/ni/measurements/data/v1/client/_client.py
+++ packages/ni.measurements.data.v1.client/src/ni/measurements/data/v1/client/_client.py
@@ -1,12 +1,140 @@
 """Client for accessing the NI Data Store Service."""
 
-import logging
+from typing import Optional
+
+import grpc
+import ni.measurements.data.v1.data_store_service_pb2 as data_store_service_pb2
+import ni.measurements.data.v1.data_store_service_pb2_grpc as data_store_service_pb2_grpc
+from ni.measurementlink.discovery.v1.client import DiscoveryClient
+from ni_grpc_extensions.channelpool import GrpcChannelPool
+
+from ni.measurements.data.v1.client._client_base import GrpcServiceClientBase
 
 GRPC_SERVICE_INTERFACE_NAME = "ni.measurements.data.v1.DataStoreService"
-GRPC_SERVICE_CLASS = "ni.measurements.data.v1.DataStoreService"
-
-_logger = logging.getLogger(__name__)
 
 
-class DataStoreClient:
+class DataStoreClient(GrpcServiceClientBase):
     """Client for accessing the NI Data Store Service."""
+
+    def __init__(
+        self,
+        *,
+        discovery_client: Optional[DiscoveryClient] = None,
+        grpc_channel: Optional[grpc.Channel] = None,
+        grpc_channel_pool: Optional[GrpcChannelPool] = None,
+    ) -> None:
+        """Initialize the Data Store Client.
+
+        Args:
+            discovery_client: An optional discovery client (recommended).
+
+            grpc_channel: An optional data store gRPC channel.
+
+            grpc_channel_pool: An optional gRPC channel pool (recommended).
+        """
+        super().__init__(
+            discovery_client=discovery_client,
+            grpc_channel=grpc_channel,
+            grpc_channel_pool=grpc_channel_pool,
+            service_interface_name=GRPC_SERVICE_INTERFACE_NAME,
+            service_class="",
+            stub_class=data_store_service_pb2_grpc.DataStoreServiceStub,
+        )
+
+    def create_session(
+        self, request: data_store_service_pb2.CreateSessionRequest
+    ) -> data_store_service_pb2.CreateSessionResponse:
+        """Creates a new session in the data store."""
+        stub = self._get_stub()
+        return stub.CreateSession(request)
+
+    def get_session(
+        self, request: data_store_service_pb2.GetSessionRequest
+    ) -> data_store_service_pb2.GetSessionResponse:
+        """Gets the session associated with the identifier given in the request."""
+        stub = self._get_stub()
+        return stub.GetSession(request)
+
+    def create_measurement(
+        self, request: data_store_service_pb2.CreateMeasurementRequest
+    ) -> data_store_service_pb2.CreateMeasurementResponse:
+        """Creates a new measurement in the data store."""
+        stub = self._get_stub()
+        return stub.CreateMeasurement(request)
+
+    def get_measurement(
+        self, request: data_store_service_pb2.GetMeasurementRequest
+    ) -> data_store_service_pb2.GetMeasurementResponse:
+        """Gets the measurement associated with the identifier given in the request."""
+        stub = self._get_stub()
+        return stub.GetMeasurement(request)
+
+    def query_measurements(
+        self, request: data_store_service_pb2.QueryMeasurementsRequest
+    ) -> data_store_service_pb2.QueryMeasurementsResponse:
+        """Query for measurements matching the given OData query."""
+        stub = self._get_stub()
+        return stub.QueryMeasurements(request)
+
+    def publish_condition_set(
+        self, request: data_store_service_pb2.PublishConditionSetRequest
+    ) -> data_store_service_pb2.PublishConditionSetResponse:
+        """Publishes a single condition set for a measurement."""
+        stub = self._get_stub()
+        return stub.PublishConditionSet(request)
+
+    def publish_condition(
+        self, request: data_store_service_pb2.PublishConditionRequest
+    ) -> data_store_service_pb2.PublishConditionResponse:
+        """Publishes a single condition value for a measurement."""
+        stub = self._get_stub()
+        return stub.PublishCondition(request)
+
+    def publish_condition_set_batch(
+        self, request: data_store_service_pb2.PublishConditionSetBatchRequest
+    ) -> data_store_service_pb2.PublishConditionSetBatchResponse:
+        """Publishes the complete set of conditions for a measurement."""
+        stub = self._get_stub()
+        return stub.PublishConditionSetBatch(request)
+
+    def publish_condition_batch(
+        self, request: data_store_service_pb2.PublishConditionBatchRequest
+    ) -> data_store_service_pb2.PublishConditionBatchResponse:
+        """Publishes a batch of condition values for a measurement."""
+        stub = self._get_stub()
+        return stub.PublishConditionBatch(request)
+
+    def publish_data(
+        self, request: data_store_service_pb2.PublishDataRequest
+    ) -> data_store_service_pb2.PublishDataResponse:
+        """Publishes a single data value."""
+        stub = self._get_stub()
+        return stub.PublishData(request)
+
+    def publish_data_batch(
+        self, request: data_store_service_pb2.PublishDataBatchRequest
+    ) -> data_store_service_pb2.PublishDataBatchResponse:
+        """Publishes a batch of scalar data values."""
+        stub = self._get_stub()
+        return stub.PublishDataBatch(request)
+
+    def query_condition_sets(
+        self, request: data_store_service_pb2.QueryConditionSetsRequest
+    ) -> data_store_service_pb2.QueryConditionSetsResponse:
+        """Queries the condition sets in the data store, based on an OData query."""
+        stub = self._get_stub()
+        return stub.QueryConditionSets(request)
+
+    def query_conditions(
+        self, request: data_store_service_pb2.QueryConditionsRequest
+    ) -> data_store_service_pb2.QueryConditionsResponse:
+        """Queries the conditions in the data store, based on an OData query."""
+        stub = self._get_stub()
+        return stub.QueryConditions(request)
+
+    def query_data(
+        self, request: data_store_service_pb2.QueryDataRequest
+    ) -> data_store_service_pb2.QueryDataResponse:
+        """Queries the data in the data store, based on an OData query."""
+        stub = self._get_stub()
+        return stub.QueryData(request)
--- packages/ni.measurements.metadata.v1.client/src/ni/measurements/metadata/v1/client/_client.py
+++ packages/ni.measurements.metadata.v1.client/src/ni/measurements/metadata/v1/client/_client.py
@@ -1,12 +1,255 @@
 """Client for accessing the NI Metadata Store Service."""
 
-import logging
+from typing import Optional
+
+import grpc
+import ni.measurements.metadata.v1.metadata_store_service_pb2 as metadata_store_service_pb2
+import ni.measurements.metadata.v1.metadata_store_service_pb2_grpc as metadata_store_service_pb2_grpc
+from ni.measurementlink.discovery.v1.client import DiscoveryClient
+from ni_grpc_extensions.channelpool import GrpcChannelPool
+
+from ni.measurements.metadata.v1.client._client_base import GrpcServiceClientBase
 
 GRPC_SERVICE_INTERFACE_NAME = "ni.measurements.metadata.v1.MetadataStoreService"
-GRPC_SERVICE_CLASS = "ni.measurements.data.v1.DataStoreService"
-
-_logger = logging.getLogger(__name__)
 
 
-class MetadataStoreClient:
+class MetadataStoreClient(GrpcServiceClientBase):
     """Client for accessing the NI Metadata Store Service."""
+
+    def __init__(
+        self,
+        *,
+        discovery_client: Optional[DiscoveryClient] = None,
+        grpc_channel: Optional[grpc.Channel] = None,
+        grpc_channel_pool: Optional[GrpcChannelPool] = None,
+    ) -> None:
+        """Initialize the Metadata Store Client.
+
+        Args:
+            discovery_client: An optional discovery client (recommended).
+
+            grpc_channel: An optional metadata store gRPC channel.
+
+            grpc_channel_pool: An optional gRPC channel pool (recommended).
+        """
+        super().__init__(
+            discovery_client=discovery_client,
+            grpc_channel=grpc_channel,
+            grpc_channel_pool=grpc_channel_pool,
+            service_interface_name=GRPC_SERVICE_INTERFACE_NAME,
+            service_class="",
+            stub_class=metadata_store_service_pb2_grpc.MetadataStoreServiceStub,
+        )
+
+    def get_dut(
+        self, request: metadata_store_service_pb2.GetDutRequest
+    ) -> metadata_store_service_pb2.GetDutResponse:
+        """Gets the device under test associated with the identifier given in the request."""
+        stub = self._get_stub()
+        return stub.GetDut(request)
+
+    def query_duts(
+        self, request: metadata_store_service_pb2.QueryDutsRequest
+    ) -> metadata_store_service_pb2.QueryDutsResponse:
+        """Perform an OData query on DUTs."""
+        stub = self._get_stub()
+        return stub.QueryDuts(request)
+
+    def create_dut(
+        self, request: metadata_store_service_pb2.CreateDutRequest
+    ) -> metadata_store_service_pb2.CreateDutResponse:
+        """Creates a new device under test in the metadata store."""
+        stub = self._get_stub()
+        return stub.CreateDut(request)
+
+    def get_product(
+        self, request: metadata_store_service_pb2.GetProductRequest
+    ) -> metadata_store_service_pb2.GetProductResponse:
+        """Gets the product associated with the identifier given in the request."""
+        stub = self._get_stub()
+        return stub.GetProduct(request)
+
+    def query_products(
+        self, request: metadata_store_service_pb2.QueryProductsRequest
+    ) -> metadata_store_service_pb2.QueryProductsResponse:
+        """Perform an OData query on products."""
+        stub = self._get_stub()
+        return stub.QueryProducts(request)
+
+    def create_product(
+        self, request: metadata_store_service_pb2.CreateProductRequest
+    ) -> metadata_store_service_pb2.CreateProductResponse:
+        """Creates a new product in the metadata store."""
+        stub = self._get_stub()
+        return stub.CreateProduct(request)
+
+    def get_operator(
+        self, request: metadata_store_service_pb2.GetOperatorRequest
+    ) -> metadata_store_service_pb2.GetOperatorResponse:
+        """Gets the operator associated with the identifier given in the request."""
+        stub = self._get_stub()
+        return stub.GetOperator(request)
+
+    def query_operators(
+        self, request: metadata_store_service_pb2.QueryOperatorsRequest
+    ) -> metadata_store_service_pb2.QueryOperatorsResponse:
+        """Perform an OData query on operators."""
+        stub = self._get_stub()
+        return stub.QueryOperators(request)
+
+    def create_operator(
+        self, request: metadata_store_service_pb2.CreateOperatorRequest
+    ) -> metadata_store_service_pb2.CreateOperatorResponse:
+        """Creates a new operator in the metadata store."""
+        stub = self._get_stub()
+        return stub.CreateOperator(request)
+
+    def get_test_plan(
+        self, request: metadata_store_service_pb2.GetTestPlanRequest
+    ) -> metadata_store_service_pb2.GetTestPlanResponse:
+        """Gets the test plan associated with the identifier given in the request."""
+        stub = self._get_stub()
+        return stub.GetTestPlan(request)
+
+    def query_test_plans(
+        self, request: metadata_store_service_pb2.QueryTestPlansRequest
+    ) -> metadata_store_service_pb2.QueryTestPlansResponse:
+        """Perform an OData query on test plans."""
+        stub = self._get_stub()
+        return stub.QueryTestPlans(request)
+
+    def create_test_plan(
+        self, request: metadata_store_service_pb2.CreateTestPlanRequest
+    ) -> metadata_store_service_pb2.CreateTestPlanResponse:
+        """Creates a new test plan in the metadata store."""
+        stub = self._get_stub()
+        return stub.CreateTestPlan(request)
+
+    def get_test(
+        self, request: metadata_store_service_pb2.GetTestRequest
+    ) -> metadata_store_service_pb2.GetTestResponse:
+        """Gets the test associated with the identifier given in the request."""
+        stub = self._get_stub()
+        return stub.GetTest(request)
+
+    def query_tests(
+        self, request: metadata_store_service_pb2.QueryTestsRequest
+    ) -> metadata_store_service_pb2.QueryTestsResponse:
+        """Perform an OData query on tests."""
+        stub = self._get_stub()
+        return stub.QueryTests(request)
+
+    def create_test(
+        self, request: metadata_store_service_pb2.CreateTestRequest
+    ) -> metadata_store_service_pb2.CreateTestResponse:
+        """Creates a new test in the metadata store."""
+        stub = self._get_stub()
+        return stub.CreateTest(request)
+
+    def get_test_station(
+        self, request: metadata_store_service_pb2.GetTestStationRequest
+    ) -> metadata_store_service_pb2.GetTestStationResponse:
+        """Gets the test station associated with the identifier given in the request."""
+        stub = self._get_stub()
+        return stub.GetTestStation(request)
+
+    def query_test_stations(
+        self, request: metadata_store_service_pb2.QueryTestStationsRequest
+    ) -> metadata_store_service_pb2.QueryTestStationsResponse:
+        """Perform an OData query on test stations."""
+        stub = self._get_stub()
+        return stub.QueryTestStations(request)
+
+    def create_test_station(
+        self, request: metadata_store_service_pb2.CreateTestStationRequest
+    ) -> metadata_store_service_pb2.CreateTestStationResponse:
+        """Creates a new test station in the metadata store."""
+        stub = self._get_stub()
+        return stub.CreateTestStation(request)
+
+    def get_hardware(
+        self, request: metadata_store_service_pb2.GetHardwareRequest
+    ) -> metadata_store_service_pb2.GetHardwareResponse:
+        """Gets the hardware associated with the identifier given in the request."""
+        stub = self._get_stub()
+        return stub.GetHardware(request)
+
+    def query_hardware(
+        self, request: metadata_store_service_pb2.QueryHardwareRequest
+    ) -> metadata_store_service_pb2.QueryHardwareResponse:
+        """Perform an OData query on hardware."""
+        stub = self._get_stub()
+        return stub.QueryHardware(request)
+
+    def create_hardware(
+        self, request: metadata_store_service_pb2.CreateHardwareRequest
+    ) -> metadata_store_service_pb2.CreateHardwareResponse:
+        """Creates new hardware in the metadata store."""
+        stub = self._get_stub()
+        return stub.CreateHardware(request)
+
+    def get_software(
+        self, request: metadata_store_service_pb2.GetSoftwareRequest
+    ) -> metadata_store_service_pb2.GetSoftwareResponse:
+        """Gets the software associated with the identifier given in the request."""
+        stub = self._get_stub()
+        return stub.GetSoftware(request)
+
+    def query_software(
+        self, request: metadata_store_service_pb2.QuerySoftwareRequest
+    ) -> metadata_store_service_pb2.QuerySoftwareResponse:
+        """Perform an OData query on software."""
+        stub = self._get_stub()
+        return stub.QuerySoftware(request)
+
+    def create_software(
+        self, request: metadata_store_service_pb2.CreateSoftwareRequest
+    ) -> metadata_store_service_pb2.CreateSoftwareResponse:
+        """Creates new software in the metadata store."""
+        stub = self._get_stub()
+        return stub.CreateSoftware(request)
+
+    def register_metadata_schema(
+        self, request: metadata_store_service_pb2.RegisterMetadataSchemaRequest
+    ) -> metadata_store_service_pb2.RegisterMetadataSchemaResponse:
+        """Registers a metadata schema."""
+        stub = self._get_stub()
+        return stub.RegisterMetadataSchema(request)
+
+    def enumerate_metadata_schemas(
+        self, request: metadata_store_service_pb2.EnumerateMetadataSchemasRequest
+    ) -> metadata_store_service_pb2.EnumerateMetadataSchemasResponse:
+        """Enumerate the metadata schemas that have been previously registered."""
+        stub = self._get_stub()
+        return stub.EnumerateMetadataSchemas(request)
+
+    def resolve_alias(
+        self, request: metadata_store_service_pb2.ResolveAliasRequest
+    ) -> metadata_store_service_pb2.ResolveAliasResponse:
+        """Resolves a given alias to its target."""
+        stub = self._get_stub()
+        return stub.ResolveAlias(request)
+
+    def query_aliases(
+        self, request: metadata_store_service_pb2.QueryAliasesRequest
+    ) -> metadata_store_service_pb2.QueryAliasesResponse:
+        """Perform an OData query on the registered aliases."""
+        stub = self._get_stub()
+        return stub.QueryAliases(request)
+
+    def register_alias(
+        self, request: metadata_store_service_pb2.RegisterAliasRequest
+    ) -> metadata_store_service_pb2.RegisterAliasResponse:
+        """Registers an alias of the specified metadata.
+
+        This alias can be used when creating other metadata or publishing.
+        """
+        stub = self._get_stub()
+        return stub.RegisterAlias(request)
+
+    def unregister_alias(
+        self, request: metadata_store_service_pb2.UnregisterAliasRequest
+    ) -> metadata_store_service_pb2.UnregisterAliasResponse:
+        """Removes a registered alias."""
+        stub = self._get_stub()
+        return stub.UnregisterAlias(request)
