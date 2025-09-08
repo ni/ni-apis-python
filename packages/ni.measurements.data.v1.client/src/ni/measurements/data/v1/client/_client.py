c00de6afb326967959c0687949c3efa78b7d2fae
hunter-ni <68388297+hunter-ni@users.noreply.github.com>
2025-09-08 15:09:54 -0500

    Addressing CI failures

--- packages/ni.measurements.data.v1.client/src/ni/measurements/data/v1/client/_client.py
+++ packages/ni.measurements.data.v1.client/src/ni/measurements/data/v1/client/_client.py
@@ -45,96 +45,96 @@ class DataStoreClient(GrpcServiceClientBase):
         self, request: data_store_service_pb2.CreateSessionRequest
     ) -> data_store_service_pb2.CreateSessionResponse:
         """Creates a new session in the data store."""
-        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()
+        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.CreateSession(request)
 
     def get_session(
         self, request: data_store_service_pb2.GetSessionRequest
     ) -> data_store_service_pb2.GetSessionResponse:
         """Gets the session associated with the identifier given in the request."""
-        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()
+        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.GetSession(request)
 
     def create_measurement(
         self, request: data_store_service_pb2.CreateMeasurementRequest
     ) -> data_store_service_pb2.CreateMeasurementResponse:
         """Creates a new measurement in the data store."""
-        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()
+        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.CreateMeasurement(request)
 
     def get_measurement(
         self, request: data_store_service_pb2.GetMeasurementRequest
     ) -> data_store_service_pb2.GetMeasurementResponse:
         """Gets the measurement associated with the identifier given in the request."""
-        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()
+        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.GetMeasurement(request)
 
     def query_measurements(
         self, request: data_store_service_pb2.QueryMeasurementsRequest
     ) -> data_store_service_pb2.QueryMeasurementsResponse:
         """Query for measurements matching the given OData query."""
-        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()
+        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.QueryMeasurements(request)
 
     def publish_condition_set(
         self, request: data_store_service_pb2.PublishConditionSetRequest
     ) -> data_store_service_pb2.PublishConditionSetResponse:
         """Publishes a single condition set for a measurement."""
-        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()
+        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.PublishConditionSet(request)
 
     def publish_condition(
         self, request: data_store_service_pb2.PublishConditionRequest
     ) -> data_store_service_pb2.PublishConditionResponse:
         """Publishes a single condition value for a measurement."""
-        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()
+        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.PublishCondition(request)
 
     def publish_condition_set_batch(
         self, request: data_store_service_pb2.PublishConditionSetBatchRequest
     ) -> data_store_service_pb2.PublishConditionSetBatchResponse:
         """Publishes the complete set of conditions for a measurement."""
-        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()
+        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.PublishConditionSetBatch(request)
 
     def publish_condition_batch(
         self, request: data_store_service_pb2.PublishConditionBatchRequest
     ) -> data_store_service_pb2.PublishConditionBatchResponse:
         """Publishes a batch of condition values for a measurement."""
-        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()
+        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.PublishConditionBatch(request)
 
     def publish_data(
         self, request: data_store_service_pb2.PublishDataRequest
     ) -> data_store_service_pb2.PublishDataResponse:
         """Publishes a single data value."""
-        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()
+        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.PublishData(request)
 
     def publish_data_batch(
         self, request: data_store_service_pb2.PublishDataBatchRequest
     ) -> data_store_service_pb2.PublishDataBatchResponse:
         """Publishes a batch of scalar data values."""
-        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()
+        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.PublishDataBatch(request)
 
     def query_condition_sets(
         self, request: data_store_service_pb2.QueryConditionSetsRequest
     ) -> data_store_service_pb2.QueryConditionSetsResponse:
         """Queries the condition sets in the data store, based on an OData query."""
-        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()
+        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.QueryConditionSets(request)
 
     def query_conditions(
         self, request: data_store_service_pb2.QueryConditionsRequest
     ) -> data_store_service_pb2.QueryConditionsResponse:
         """Queries the conditions in the data store, based on an OData query."""
-        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()
+        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.QueryConditions(request)
 
     def query_data(
         self, request: data_store_service_pb2.QueryDataRequest
     ) -> data_store_service_pb2.QueryDataResponse:
         """Queries the data in the data store, based on an OData query."""
-        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()
+        stub: data_store_service_pb2_grpc.DataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.QueryData(request)
--- packages/ni.measurements.metadata.v1.client/src/ni/measurements/metadata/v1/client/_client.py
+++ packages/ni.measurements.metadata.v1.client/src/ni/measurements/metadata/v1/client/_client.py
@@ -45,196 +45,196 @@ class MetadataStoreClient(GrpcServiceClientBase):
         self, request: metadata_store_service_pb2.GetDutRequest
     ) -> metadata_store_service_pb2.GetDutResponse:
         """Gets the device under test associated with the identifier given in the request."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.GetDut(request)
 
     def query_duts(
         self, request: metadata_store_service_pb2.QueryDutsRequest
     ) -> metadata_store_service_pb2.QueryDutsResponse:
         """Perform an OData query on DUTs."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.QueryDuts(request)
 
     def create_dut(
         self, request: metadata_store_service_pb2.CreateDutRequest
     ) -> metadata_store_service_pb2.CreateDutResponse:
         """Creates a new device under test in the metadata store."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.CreateDut(request)
 
     def get_product(
         self, request: metadata_store_service_pb2.GetProductRequest
     ) -> metadata_store_service_pb2.GetProductResponse:
         """Gets the product associated with the identifier given in the request."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.GetProduct(request)
 
     def query_products(
         self, request: metadata_store_service_pb2.QueryProductsRequest
     ) -> metadata_store_service_pb2.QueryProductsResponse:
         """Perform an OData query on products."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.QueryProducts(request)
 
     def create_product(
         self, request: metadata_store_service_pb2.CreateProductRequest
     ) -> metadata_store_service_pb2.CreateProductResponse:
         """Creates a new product in the metadata store."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.CreateProduct(request)
 
     def get_operator(
         self, request: metadata_store_service_pb2.GetOperatorRequest
     ) -> metadata_store_service_pb2.GetOperatorResponse:
         """Gets the operator associated with the identifier given in the request."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.GetOperator(request)
 
     def query_operators(
         self, request: metadata_store_service_pb2.QueryOperatorsRequest
     ) -> metadata_store_service_pb2.QueryOperatorsResponse:
         """Perform an OData query on operators."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.QueryOperators(request)
 
     def create_operator(
         self, request: metadata_store_service_pb2.CreateOperatorRequest
     ) -> metadata_store_service_pb2.CreateOperatorResponse:
         """Creates a new operator in the metadata store."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.CreateOperator(request)
 
     def get_test_plan(
         self, request: metadata_store_service_pb2.GetTestPlanRequest
     ) -> metadata_store_service_pb2.GetTestPlanResponse:
         """Gets the test plan associated with the identifier given in the request."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.GetTestPlan(request)
 
     def query_test_plans(
         self, request: metadata_store_service_pb2.QueryTestPlansRequest
     ) -> metadata_store_service_pb2.QueryTestPlansResponse:
         """Perform an OData query on test plans."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.QueryTestPlans(request)
 
     def create_test_plan(
         self, request: metadata_store_service_pb2.CreateTestPlanRequest
     ) -> metadata_store_service_pb2.CreateTestPlanResponse:
         """Creates a new test plan in the metadata store."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.CreateTestPlan(request)
 
     def get_test(
         self, request: metadata_store_service_pb2.GetTestRequest
     ) -> metadata_store_service_pb2.GetTestResponse:
         """Gets the test associated with the identifier given in the request."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.GetTest(request)
 
     def query_tests(
         self, request: metadata_store_service_pb2.QueryTestsRequest
     ) -> metadata_store_service_pb2.QueryTestsResponse:
         """Perform an OData query on tests."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.QueryTests(request)
 
     def create_test(
         self, request: metadata_store_service_pb2.CreateTestRequest
     ) -> metadata_store_service_pb2.CreateTestResponse:
         """Creates a new test in the metadata store."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.CreateTest(request)
 
     def get_test_station(
         self, request: metadata_store_service_pb2.GetTestStationRequest
     ) -> metadata_store_service_pb2.GetTestStationResponse:
         """Gets the test station associated with the identifier given in the request."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.GetTestStation(request)
 
     def query_test_stations(
         self, request: metadata_store_service_pb2.QueryTestStationsRequest
     ) -> metadata_store_service_pb2.QueryTestStationsResponse:
         """Perform an OData query on test stations."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.QueryTestStations(request)
 
     def create_test_station(
         self, request: metadata_store_service_pb2.CreateTestStationRequest
     ) -> metadata_store_service_pb2.CreateTestStationResponse:
         """Creates a new test station in the metadata store."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.CreateTestStation(request)
 
     def get_hardware(
         self, request: metadata_store_service_pb2.GetHardwareRequest
     ) -> metadata_store_service_pb2.GetHardwareResponse:
         """Gets the hardware associated with the identifier given in the request."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.GetHardware(request)
 
     def query_hardware(
         self, request: metadata_store_service_pb2.QueryHardwareRequest
     ) -> metadata_store_service_pb2.QueryHardwareResponse:
         """Perform an OData query on hardware."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.QueryHardware(request)
 
     def create_hardware(
         self, request: metadata_store_service_pb2.CreateHardwareRequest
     ) -> metadata_store_service_pb2.CreateHardwareResponse:
         """Creates new hardware in the metadata store."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.CreateHardware(request)
 
     def get_software(
         self, request: metadata_store_service_pb2.GetSoftwareRequest
     ) -> metadata_store_service_pb2.GetSoftwareResponse:
         """Gets the software associated with the identifier given in the request."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.GetSoftware(request)
 
     def query_software(
         self, request: metadata_store_service_pb2.QuerySoftwareRequest
     ) -> metadata_store_service_pb2.QuerySoftwareResponse:
         """Perform an OData query on software."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.QuerySoftware(request)
 
     def create_software(
         self, request: metadata_store_service_pb2.CreateSoftwareRequest
     ) -> metadata_store_service_pb2.CreateSoftwareResponse:
         """Creates new software in the metadata store."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.CreateSoftware(request)
 
     def register_metadata_schema(
         self, request: metadata_store_service_pb2.RegisterMetadataSchemaRequest
     ) -> metadata_store_service_pb2.RegisterMetadataSchemaResponse:
         """Registers a metadata schema."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.RegisterMetadataSchema(request)
 
     def enumerate_metadata_schemas(
         self, request: metadata_store_service_pb2.EnumerateMetadataSchemasRequest
     ) -> metadata_store_service_pb2.EnumerateMetadataSchemasResponse:
         """Enumerate the metadata schemas that have been previously registered."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.EnumerateMetadataSchemas(request)
 
     def resolve_alias(
         self, request: metadata_store_service_pb2.ResolveAliasRequest
     ) -> metadata_store_service_pb2.ResolveAliasResponse:
         """Resolves a given alias to its target."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.ResolveAlias(request)
 
     def query_aliases(
         self, request: metadata_store_service_pb2.QueryAliasesRequest
     ) -> metadata_store_service_pb2.QueryAliasesResponse:
         """Perform an OData query on the registered aliases."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.QueryAliases(request)
 
     def register_alias(
@@ -244,12 +244,12 @@ class MetadataStoreClient(GrpcServiceClientBase):
 
         This alias can be used when creating other metadata or publishing.
         """
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.RegisterAlias(request)
 
     def unregister_alias(
         self, request: metadata_store_service_pb2.UnregisterAliasRequest
     ) -> metadata_store_service_pb2.UnregisterAliasResponse:
         """Removes a registered alias."""
-        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()
+        stub: metadata_store_service_pb2_grpc.MetadataStoreServiceStub = self._get_stub()  # type: ignore
         return stub.UnregisterAlias(request)
