9500cdaa573eebf2ad911804efb7678b01110925
hunter-ni <68388297+hunter-ni@users.noreply.github.com>
2025-09-08 16:02:33 -0500

    Adding initial implementation of MonikerClient

--- packages/ni.datamonikers.v1.client/src/ni/datamonikers/v1/client/_client.py
+++ packages/ni.datamonikers.v1.client/src/ni/datamonikers/v1/client/_client.py
@@ -1,12 +1,97 @@
 """Client for accessing the NI Data Moniker Service."""
 
-import logging
+import threading
+from typing import Generator, Optional
 
-GRPC_SERVICE_INTERFACE_NAME = "ni.datamonikers.v1.MonikerService"
-GRPC_SERVICE_CLASS = "ni.measurements.data.v1.DataStoreService"
-
-_logger = logging.getLogger(__name__)
+import grpc
+import ni.datamonikers.v1.data_moniker_pb2 as data_moniker_pb2
+import ni.datamonikers.v1.data_moniker_pb2_grpc as data_moniker_pb2_grpc
+from ni_grpc_extensions.channelpool import GrpcChannelPool
 
 
 class MonikerClient:
     """Client for accessing the NI Data Moniker Service."""
+
+    def __init__(
+        self,
+        *,
+        uri: Optional[str] = None,
+        grpc_channel: Optional[grpc.Channel] = None,
+        grpc_channel_pool: Optional[GrpcChannelPool] = None,
+    ) -> None:
+        """Initialize the Moniker Client.
+
+        Args:
+            uri: A URI of the Data Moniker service location.
+
+            grpc_channel: A data moniker gRPC channel.
+
+            grpc_channel_pool: An optional gRPC channel pool (recommended).
+
+        Either `uri` or `grpc_channel` must be provided. If both are provided,
+        `grpc_channel` takes precedence.
+        """
+        if uri is None and grpc_channel is None:
+            raise ValueError("Either 'uri' or 'grpc_channel' must be provided.")
+
+        self._initialization_lock = threading.Lock()
+        self._uri = uri
+        self._grpc_channel_pool = grpc_channel_pool
+        self._stub = (
+            data_moniker_pb2_grpc.MonikerServiceStub(grpc_channel)
+            if grpc_channel is not None
+            else None
+        )
+
+    def _get_stub(self) -> data_moniker_pb2_grpc.MonikerServiceStub:
+        if self._stub is None:
+            with self._initialization_lock:
+                if self._grpc_channel_pool is None:
+                    self._grpc_channel_pool = GrpcChannelPool()
+
+                channel = self._grpc_channel_pool.get_channel(self._uri)
+                self._stub = data_moniker_pb2_grpc.MonikerServiceStub(channel)
+
+        return self._stub
+
+    def begin_sideband_stream(
+        self, request: data_moniker_pb2.BeginMonikerSidebandStreamRequest
+    ) -> data_moniker_pb2.BeginMonikerSidebandStreamResponse:
+        """Begin a sideband stream."""
+        stub = self._get_stub()
+        return stub.BeginSidebandStream(request)
+
+    def stream_read(
+        self, moniker_list: data_moniker_pb2.MonikerList
+    ) -> Generator[data_moniker_pb2.MonikerReadResult, None, None]:
+        """Stream read data from monikers."""
+        stub = self._get_stub()
+        return stub.StreamRead(moniker_list)
+
+    def stream_write(
+        self, requests: Generator[data_moniker_pb2.MonikerWriteRequest, None, None]
+    ) -> Generator[data_moniker_pb2.StreamWriteResponse, None, None]:
+        """Stream write data to monikers."""
+        stub = self._get_stub()
+        return stub.StreamWrite(requests)
+
+    def stream_read_write(
+        self, requests: Generator[data_moniker_pb2.MonikerWriteRequest, None, None]
+    ) -> Generator[data_moniker_pb2.MonikerReadResult, None, None]:
+        """Stream read and write data with monikers."""
+        stub = self._get_stub()
+        return stub.StreamReadWrite(requests)
+
+    def read_from_moniker(
+        self, moniker: data_moniker_pb2.Moniker
+    ) -> data_moniker_pb2.ReadFromMonikerResult:
+        """Read data from a moniker."""
+        stub = self._get_stub()
+        return stub.ReadFromMoniker(moniker)
+
+    def write_to_moniker(
+        self, request: data_moniker_pb2.WriteToMonikerRequest
+    ) -> data_moniker_pb2.WriteToMonikerResponse:
+        """Write data to a moniker."""
+        stub = self._get_stub()
+        return stub.WriteToMoniker(request)
