f48b045fdc063144fe8a2e919128535dbae2d2d2
Joel Dixon <joel.dixon@ni.com>
2025-09-09 12:01:36 -0500

    Fix _client.py comments

--- packages/ni.measurementlink.pinmap.v1.client/src/ni/measurementlink/pinmap/v1/client/_client.py
+++ packages/ni.measurementlink.pinmap.v1.client/src/ni/measurementlink/pinmap/v1/client/_client.py
@@ -23,7 +23,15 @@ class PinMapClient(GrpcServiceClientBase):
         grpc_channel: grpc.Channel | None = None,
         grpc_channel_pool: GrpcChannelPool | None = None,
     ) -> None:
-        """Initialize a PinMapClient instance."""
+        """Initialize the pin map client.
+
+        Args:
+            discovery_client: An optional discovery client (recommended).
+
+            grpc_channel: An optional pin map gRPC channel.
+
+            grpc_channel_pool: An optional gRPC channel pool (recommended).
+        """
         super().__init__(
             discovery_client=discovery_client,
             grpc_channel=grpc_channel,
@@ -34,11 +42,21 @@ class PinMapClient(GrpcServiceClientBase):
         )
 
     def update_pin_map(self, pin_map_path: str | pathlib.Path) -> str:
-        """Update the pin map from an XML file and return the pin map ID."""
+        """Update registered pin map contents.
+
+        Create and register a pin map if a pin map resource for the specified pin map id is not
+        found.
+
+        Args:
+            pin_map_path: The file path of the pin map to register as a pin map resource.
+
+        Returns:
+            The resource id of the pin map that is registered to the pin map service.
+        """
         request = pin_map_service_pb2.UpdatePinMapFromXmlRequest(
             pin_map_id=str(pin_map_path),
             pin_map_xml=pathlib.Path(pin_map_path).read_text(encoding="utf-8-sig"),
         )
         stub: pin_map_service_pb2_grpc.PinMapServiceStub = self._get_stub()  # type: ignore
         response = stub.UpdatePinMapFromXml(request)
-        return str(response.pin_map_id)
+        return response.pin_map_id
